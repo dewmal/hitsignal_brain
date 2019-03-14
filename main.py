c6a1401737bdd723cead28b7583bf8676f92c2fc
dewmal <dewmalnilanka@gmail.com>
2019-03-15 03:39:26 +0530

    Init Settings

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from mode import Service
+
+import settings
 
 
+class MainAppService(Service):
+    async def on_start(self) -> None:
+        print('APP STARTING')
+        import pydot
+        import io
+        o = io.StringIO()
+        beacon = self.beacon.root or self.beacon
+        beacon.as_graph().to_dot(o)
+        graph, = pydot.graph_from_dot_data(o.getvalue())
+        print('WRITING GRAPH TO image.png')
+        with open(f'{settings.TEMP_FOLDER}/image.png', 'wb') as fh:
+            fh.write(graph.create_png())
 
-# asyncio.get_event_loop().run_until_complete(read())
+
+if __name__ == '__main__':
+    from mode import Worker
+
+    Worker(MainAppService(), loglevel="info").execute_from_commandline()
---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+
+dirname = os.path.dirname(__file__)
+base_path = dirname + ""
+
+# tmp
+TEMP_FOLDER = f"{base_path}/tmp"
+
+# models
+MODEL_FOLDER = f"{base_path}/models"
+
+# data
+DATA_FOLDER = f"{base_path}/data"
