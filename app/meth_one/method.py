ebe149d841cd06404249af8b598e74a26ee7ce12
dewmal <dewmalnilanka@gmail.com>
2019-03-16 00:47:58 +0530

    Publish predictions and streams to sio

--- app/meth_one/method.py
+++ app/meth_one/method.py
@@ -4,7 +4,7 @@ from mode.threads import ServiceThread
 
 import settings
 from app.meth_one.stream import fx_message_stream_topic_eur_usd, FxStreamMessage, fx_message_live_stream_topic, \
-    fx_price_prediction_stream, fx_candle_stream, Candle, fx_candle_1min_stream
+    fx_price_prediction_stream, fx_candle_stream, Candle, fx_candle_1min_stream, PricePrediction
 from app.meth_one.utils import read_from_ws_history, read_from_ws_live, process_message
 from application import stapp
 from utils.stream_helper import stream_last_
@@ -13,7 +13,6 @@ m_sio = socketio.AsyncRedisManager(settings.SOCKET_IO_MESSAGE_BROKER, write_only
 
 
 class MethodOne(ServiceThread):
-    from . import processors, stream
 
     async def on_start(self) -> None:
         print('MethodOne STARTING')
@@ -46,7 +45,6 @@ async def call_after_process_candle_live_feed(candle):
 
 @stream_last_(fx_candle_stream, stapp)
 async def candle_stream_fx(stream):
-    print(f"Stream -  {stream}")
     await fx_candle_1min_stream.send(value=stream)
 
 
@@ -54,6 +52,24 @@ async def candle_stream_fx(stream):
 async def process_bar_data(views):
     async for fx_msg in views:
         await process_message(fx_msg.message, call_back=call_after_process_candle_live_feed)
+
+
+@stapp.agent(fx_candle_1min_stream)
+async def live_stream_candle_fx_data(messages):
+    async  for candle in messages:
+        candle: Candle = candle
+        print(candle)
+        await m_sio.emit('candle_live_stream_eur_usd',
+                         {
+                             "open": candle.open,
+                             "close": candle.close,
+                             "high": candle.high,
+                             "low": candle.low,
+                             "date": candle.open_time,
+                             "close_time": candle.epoch
+                         },
+                         namespace='/trading'
+                         )
 
 
 @stapp.agent(fx_message_live_stream_topic)
@@ -65,7 +81,18 @@ async def live_stream_fx_data(messages):
                          namespace='/trading'
                          )
 
+
 @stapp.agent(fx_price_prediction_stream)
 async def fx_price_prediction_stream_broad_cast(predictions):
     async for prediction in predictions:
-        print(prediction)
+        prediction: PricePrediction = prediction
+        await m_sio.emit('price_prediction',
+                         {
+                             "time": prediction.time,
+                             "price": prediction.price,
+                             "price_change": prediction.price_change,
+                             "origin_time": prediction.origin_time,
+                             "originator": prediction.originator,
+                         },
+                         namespace='/trading'
+                         )
--- app/meth_one/processors.py
+++ app/meth_one/processors.py
@@ -1,12 +1,31 @@
 # data processors - Pre, Past
+import datetime
 
-from app.meth_one.stream import fx_candle_1min_stream
+from app.meth_one.stream import fx_candle_1min_stream, fx_price_prediction_stream, PricePrediction, Candle
 from application import stapp
 from utils.stream_helper import stream_window
 
 
-@stream_window(window_length=2, stream_topic=fx_candle_1min_stream, stapp=stapp)
-async def fx_candle_stream_prediction(candles):
-    print(candles)
+@stapp.agent(fx_candle_1min_stream)
+async def fx_candle_(stream):
+    async for candle in stream:
+        candle: Candle = candle
+        prediction = PricePrediction(
+            time=(datetime.datetime.fromtimestamp(candle.epoch) + datetime.timedelta(seconds=30)).timestamp(),
+            price=candle.close * (101) / 100,
+            symbol=candle.symbol,
+            price_change=candle.high / candle.open,
+            originator="random",
+            origin_time=candle.epoch
+        )
+        await fx_price_prediction_stream.send(value=prediction)
 
 
+@stream_window(window_length=20, stream_topic=fx_candle_1min_stream, stapp=stapp)
+async def fx_candle_stream_prediction_(candles):
+    window = []
+    for candle in candles:
+        ar = candle.to_olhc_array()
+        window.append(ar)
+
+    print(window)
